527f1dbbd5a0ba7039195a5e05f44b8a98276c5d
Einar <einargudnig@gmail.com>
2021-08-02 22:27:18 +0000

    add missing google analytics stufF

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,24 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
+    const GA_MEASUREMENT_ID = 'G-95HPC8T3GB'
     return (
       <Html lang="en">
         <Head>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link
             rel="icon"
